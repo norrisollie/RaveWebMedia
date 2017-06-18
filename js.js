07fcf53c579b99fe94a92087ef57f71e879f2b00
Ollie Norris <ollienorris@ollies-mbp.home>
2017-06-18 19:20:00 +0100

    Updated
    Added rellax plugin

--- js.js
+++ js.js
@@ -1,1 +1,20 @@
-js.js
+function windowDown() {
+
+
+
+var container = document.getElementById("container");
+var content = document.getElementById("content");
+var contentContainer = document.getElementById("content-container");
+
+var height = container.offsetHeight;
+
+content.style.top = 0 + "px";
+contentContainer.style.top = height + "px";
+
+}
+
+var rellax = new Rellax('.rellax');
+
+window.onload = windowDown();
+
+window.addEventListener('resize', windowDown)
